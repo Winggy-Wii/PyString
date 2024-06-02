5fede96dc7f1c960e9709440a83ae4b5ac23701c
altianman <annmail0101@gmail.com>
2024-06-03 02:26:17 +0700

    adddd

--- Pystring.cpp
+++ Pystring.cpp
@@ -66,14 +66,15 @@ public:
     }
     return result;
 
-    
-  }
 
+  }
+  //len
   int len()
   {
     return message.length();
   }
 
+  //capitalize
   std::string capitalize()
   {
     if ('a' <= message[0] && message[0] <= 'z')
@@ -94,6 +95,23 @@ public:
       }
     }
   }
+
+  //count
+  int count(const std::string& substring)
+{
+    if (substring.empty()) return 0; 
+
+    int count = 0;
+    size_t pos = message.find(substring); 
+
+    while (pos != std::string::npos)
+    {
+        count++;
+        pos = message.find(substring, pos + substring.size()); 
+    }
+
+    return count;
+}
 
 private:
   std::string message;
@@ -117,5 +135,6 @@ int main()
   // std::cout << obj1["1:3"];
   // std::cout << obj1.len();
   std::cout << obj1.capitalize();
+  std::cout << obj1.count("Hello")<<std::endl;
   return 0;
 }
